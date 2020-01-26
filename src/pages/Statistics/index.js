f4521f3c139d6949881b729ccf0a81cebba596bc
Pedro Paes <pedro_paes@apple.com>
2020-01-26 19:53:07 -0300

    Added Spinner

--- src/pages/Statistics/index.js
+++ src/pages/Statistics/index.js
@@ -8,6 +8,8 @@ import {
   BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
 } from 'recharts';
 
+import Lottie from "react-lottie";
+
 import { ResponsiveCalendar } from '@nivo/calendar'
 import { ResponsiveBar } from '@nivo/bar'
 
@@ -31,6 +33,15 @@ export default function Statistics() {
     text-align: center;
   }
 `;
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: require("../../assets/spinner.json"),
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
 
   const [data, setData] = useState([]);
   const [vehiData, setVehiData] = useState([]);
@@ -101,125 +112,135 @@ export default function Statistics() {
             </HeaderTextContainer>
           </Col>
         </Row>
-        <Row className="justify-content-md-center">
-          <Col lg="auto">
-            <Tabs activeKey={key} onSelect={k => setKey(k)} variant="statistics-tab">
-              <Tab eventKey="geral" title="Geral">
-                <Row className="justify-content-md-center">
-                  <Col lg="2" style={{ justifyContent: "center", display: "flex" }}>
-                    <Counter total={data.length} />
-                  </Col>
-                  <Col lg="6" style={{ width: '100%', height: 300 }}>
-                    <ResponsiveContainer>
-                      <BarChart
-                        data={vehiData}
-                        margin={{
-                          top: 20, right: 30, left: 20, bottom: 5,
-                        }}
-                        layout="vertical"
-                      >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis type="number" />
-                        <YAxis type="category" dataKey="name" />
-                        <Tooltip />
-                        <Legend />
-                        <Bar name="Tipo de veículo invasores" dataKey="quantity" fill="#5A31F8" />
-                      </BarChart>
-                    </ResponsiveContainer>
-                  </Col>
-                  <Col lg="6" className="mr-10" style={{ height: "400px", width: "100%" }}>
-                    <ResponsiveCalendar
-                      data={calendarData}
-                      from="2019-09-24"
-                      to="2020-01-20"
-                      emptyColor="#eeeeee"
-                      colors={['#61cdbb', '#97e3d5', '#e8c1a0', '#f47560']}
-                      margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
-                      minValue="auto"
-                      yearSpacing={40}
-                      monthBorderColor="#ffffff"
-                      dayBorderWidth={2}
-                      dayBorderColor="#ffffff"
-                      legends={[
-                        {
-                          anchor: 'bottom-left',
-                          direction: 'row',
-                          translateY: 36,
-                          itemCount: 4,
-                          itemWidth: 42,
-                          itemHeight: 36,
-                          itemsSpacing: 14,
-                          itemDirection: 'right-to-left'
-                        }
-                      ]} />
-                  </Col>
-                  <Col lg="6" className="mr-10" style={{ height: "400px", width: "100%" }}>
-                    <ResponsiveBar
-                      data={timeData}
-                      keys={['denúncias']}
-                      indexBy="hour"
-                      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-                      padding={0.3}
-                      axisTop={null}
-                      axisRight={null}
-                      axisBottom={{
-                        tickSize: 5,
-                        tickPadding: 5,
-                        tickRotation: 0,
-                        legend: 'Faixa de Horário',
-                        legendPosition: 'middle',
-                        legendOffset: 32
-                      }}
-                      axisLeft={{
-                        tickSize: 5,
-                        tickPadding: 5,
-                        tickRotation: 0,
-                        legend: 'Número de Denúncias',
-                        legendPosition: 'middle',
-                        legendOffset: -40
-                      }}
-                      labelSkipWidth={12}
-                      labelSkipHeight={12}
-                      legends={[
-                        {
-                          dataFrom: 'keys',
-                          anchor: 'bottom-right',
-                          direction: 'column',
-                          justify: false,
-                          translateX: 120,
-                          translateY: 0,
-                          itemsSpacing: 2,
-                          itemWidth: 100,
-                          itemHeight: 20,
-                          itemDirection: 'left-to-right',
-                          itemOpacity: 0.85,
-                          symbolSize: 20,
-                          effects: [
+        {isLoading ? (
+          <Lottie
+            options={defaultOptions}
+            height={400}
+            width={400}
+            isStopped={false}
+            isPaused={false}
+          />
+        ) : (
+            <Row className="justify-content-md-center">
+              <Col lg="auto">
+                <Tabs activeKey={key} onSelect={k => setKey(k)} variant="statistics-tab">
+                  <Tab eventKey="geral" title="Geral">
+                    <Row className="justify-content-md-center">
+                      <Col lg="2" style={{ justifyContent: "center", display: "flex" }}>
+                        <Counter total={data.length} />
+                      </Col>
+                      <Col lg="6" style={{ width: '100%', height: 300 }}>
+                        <ResponsiveContainer>
+                          <BarChart
+                            data={vehiData}
+                            margin={{
+                              top: 20, right: 30, left: 20, bottom: 5,
+                            }}
+                            layout="vertical"
+                          >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis type="number" />
+                            <YAxis type="category" dataKey="name" />
+                            <Tooltip />
+                            <Legend />
+                            <Bar name="Tipo de veículo invasores" dataKey="quantity" fill="#5A31F8" />
+                          </BarChart>
+                        </ResponsiveContainer>
+                      </Col>
+                      <Col lg="6" className="mr-10" style={{ height: "400px", width: "100%" }}>
+                        <ResponsiveCalendar
+                          data={calendarData}
+                          from="2019-09-24"
+                          to="2020-01-20"
+                          emptyColor="#eeeeee"
+                          colors={['#61cdbb', '#97e3d5', '#e8c1a0', '#f47560']}
+                          margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+                          minValue="auto"
+                          yearSpacing={40}
+                          monthBorderColor="#ffffff"
+                          dayBorderWidth={2}
+                          dayBorderColor="#ffffff"
+                          legends={[
                             {
-                              on: 'hover',
-                              style: {
-                                itemOpacity: 1
-                              }
+                              anchor: 'bottom-left',
+                              direction: 'row',
+                              translateY: 36,
+                              itemCount: 4,
+                              itemWidth: 42,
+                              itemHeight: 36,
+                              itemsSpacing: 14,
+                              itemDirection: 'right-to-left'
                             }
-                          ]
-                        }
-                      ]}
-                      animate={true}
-                      motionStiffness={90}
-                      motionDamping={15}
-                    />
-                  </Col>
-                </Row>
-              </Tab>
-              <Tab eventKey="sp" title="São Paulo">
-                <span>Teste2</span>
-              </Tab>
-              <Tab eventKey="rec" title="Recife">
-                <span>Teste3</span>
-              </Tab>
-            </Tabs>
-          </Col>
-        </Row>
+                          ]} />
+                      </Col>
+                      <Col lg="6" className="mr-10" style={{ height: "400px", width: "100%" }}>
+                        <ResponsiveBar
+                          data={timeData}
+                          keys={['denúncias']}
+                          indexBy="hour"
+                          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+                          padding={0.3}
+                          axisTop={null}
+                          axisRight={null}
+                          axisBottom={{
+                            tickSize: 5,
+                            tickPadding: 5,
+                            tickRotation: 0,
+                            legend: 'Faixa de Horário',
+                            legendPosition: 'middle',
+                            legendOffset: 32
+                          }}
+                          axisLeft={{
+                            tickSize: 5,
+                            tickPadding: 5,
+                            tickRotation: 0,
+                            legend: 'Número de Denúncias',
+                            legendPosition: 'middle',
+                            legendOffset: -40
+                          }}
+                          labelSkipWidth={12}
+                          labelSkipHeight={12}
+                          legends={[
+                            {
+                              dataFrom: 'keys',
+                              anchor: 'bottom-right',
+                              direction: 'column',
+                              justify: false,
+                              translateX: 120,
+                              translateY: 0,
+                              itemsSpacing: 2,
+                              itemWidth: 100,
+                              itemHeight: 20,
+                              itemDirection: 'left-to-right',
+                              itemOpacity: 0.85,
+                              symbolSize: 20,
+                              effects: [
+                                {
+                                  on: 'hover',
+                                  style: {
+                                    itemOpacity: 1
+                                  }
+                                }
+                              ]
+                            }
+                          ]}
+                          animate={true}
+                          motionStiffness={90}
+                          motionDamping={15}
+                        />
+                      </Col>
+                    </Row>
+                  </Tab>
+                  <Tab eventKey="sp" title="São Paulo">
+                    <span>Teste2</span>
+                  </Tab>
+                  <Tab eventKey="rec" title="Recife">
+                    <span>Teste3</span>
+                  </Tab>
+                </Tabs>
+              </Col>
+            </Row>
+          )}
       </Container>
     </>
   );
